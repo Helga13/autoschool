2b649f38a401bfab47175b10aa9a7a70e1a452dd
Helga13 <oliatrubina@yandex.ru>
2017-05-26 10:22:47 +0300

    index.html

---
+++ prod/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	'use strict';
+	$('.disabled').on('click', function (e) {
+		e.preventDefault();
+	});
+	// tabs
+	$('.tabs_block').each(function () {
+		$(this).find('.tab').each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();	$(this).addClass('active').siblings().removeClass('active').closest('.tabs_block').find('.tabs_content').removeClass('active').eq(i).addClass('active')
+			});
+		});
+	});
+	
+	
+	
+	
+	
+	
+	
+})
