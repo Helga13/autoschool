f78b589a25594e1028151f28266148b0fdca035c
Helga13 <oliatrubina@yandex.ru>
2017-05-31 09:56:02 +0300

    popup edit

--- prod/js/common.js
+++ prod/js/common.js
@@ -104,8 +104,5 @@ $(document).ready(function () {
 		$(sidebar).removeClass('visible');
 	});
 	
-	if($(window).innerWidth() <= 768) {
-    $('nav').addClass('media');
-	}
 	
 })
--- src/js/app.js
+++ src/js/app.js
@@ -93,8 +93,5 @@ $(document).ready(function () {
 		$(sidebar).removeClass('visible');
 	});
 	
-	if($(window).innerWidth() <= 768) {
-    $('nav').addClass('media');
-	}
 	
 })
